9f4b97e135db642c75785574c48d4b48eb17fe86
Alexferraz2 <alex_ferraz2@hotmail.com>
2024-05-23 22:28:04 -0300

    Criado a lógica para verificar o campeão

--- src/components/Game/Game.jsx
+++ src/components/Game/Game.jsx
@@ -1,27 +1,51 @@
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./Game.module.css"
 
 
 import GameOption from "../GameOption/GameOption";
 import Icons from "../Icons/Icons";
 
-
+const winnerTable = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 
 function Game() {
     const [gameSate, setGameState] = useState(Array(9).fill(0))
-
     const [currentPlayer, setCurrentPlayer] = useState(1)
+    const [winner, setWinner] = useState(0)
     
     const handleClick = (pos) => {
-        if(gameSate[pos] === 0) {
+        if(gameSate[pos] === 0 && winner === 0) {
             let newGameState = [...gameSate]
-            newGameState[pos] = currentPlayer
-            setCurrentPlayer(currentPlayer * -1)
+            newGameState[pos] = currentPlayer            
             setGameState(newGameState)
         }
         
     }
+
+    const verifyGame = () => {
+        winnerTable.forEach((line) => {
+            const values = line.map((pos) => gameSate[pos])
+            const sum = values.reduce((acc, value) => acc + value)
+            if(sum === 3 || sum === -3){
+             setWinner(sum / 3)
+            } 
+        })
+    }
+
+    useEffect(() => {
+        setCurrentPlayer(currentPlayer * -1)
+        verifyGame()
+    }, [gameSate])
+
     return(
         <div className={styles.gameContent}>
             <div className={styles.game}>
